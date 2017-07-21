9cee50489fcfe5b6f16c88e2f5de19e8ae0f0759
sinakabki <sinakabki@gmail.com>
2017-07-21 16:12:35 +0100

    reducer set up

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -4,14 +4,14 @@ const initialState = {
     currentUser: null
 };
 
-export default function reducer (prevState = initialState, action) {
+export function reducer(prevState = initialState, action) {
 
     if (action.type === types.LOG_IN_USER) {
         return Object.assign({}, prevState, {
             currentUser: action.currentUser
         });
     }
-    
+
     if (action.type === types.LOG_OUT_USER) {
         return Object.assign({}, prevState, {
             currentUser: null
@@ -21,4 +21,23 @@ export default function reducer (prevState = initialState, action) {
     else {
         return prevState;
     }
+}
+
+export function notesReducer(prevState = initialState, action) {
+
+    // implement loading on request;
+
+    if (action.type === types.GET_NOTES_SUCCESS) {
+        return Object.assign({}, prevState, {
+            data: action.data
+        });
+    }
+
+    if (action.type === types.GET_NOTES_ERROR) {
+        return Object.assign({}, prevState, {
+            data: action.data
+        });
+    }
+
+    return prevState;
 }

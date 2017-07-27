deacf0c4160cdaa0fc12bb67028724c163a32539
sinakabki <sinakabki@gmail.com>
2017-07-27 11:46:19 +0100

    add note and edit note form styling

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -4,55 +4,63 @@ import './css/AddNote.css';
 import { BrowserRouter as Router, Redirect, Link } from 'react-router-dom';
 import addNote from '../utilities/addNote.utilities';
 
+
 class AddNote extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
-            newSubmit: false
+            newSubmit: false,
+            newTitle: '',
+            touchedTitle: false
         };
         this.submitNote = this.submitNote.bind(this);
     }
-    render () {
+    render() {
         return (
-        <div>
-            {
-                this.state.newSubmit &&
-                    <Redirect to={'/notes'}/>
-            }
-            <div className='panel panel-default'>
-                <div className="panel-heading">
-                    <h3 className="panel-title">New Note</h3>
-                </div>
-                <div className="panel-body">
-                    <div className="container">
-                        <form onSubmit={this.submitNote} method="post">
-                            <div>
-                                <label htmlFor="Title">Title</label>
-                                <br />
-                                <input className="titleInput" type="text" name="Title" placeholder="title" />
-                            </div>
-                            <div>
-                                <label htmlFor="note">My Note</label>
-                                <br />
-                                <textarea className="noteInput" name="note" type="text" placeholder="write your note here..." />
-                            </div>
-                            <div>
-                                <label htmlFor="Tags">#tag</label>
-                                <br />
-                                <input name="Tags" type="text" placeholder="#" />
-                            </div>
-                            <div>
-                                <button className="btn btn-success" type="submit">Submit</button>
-                                <Link to='/notes'><button className="btn btn-warning" type="button">Cancel</button></Link>
-                            </div>
-                        </form>
+            <div>
+                {
+                    this.state.newSubmit &&
+                    <Redirect to={'/notes'} />
+                }
+                <div className='panel panel-default'>
+                    <div className="panel-heading">
+                        <h3 className="panel-title">New Note</h3>
+                    </div>
+                    <div className="panel-body">
+                        <div className="container">
+                            <form onSubmit={this.submitNote} method="post">
+                                <div className="form-group row">
+                                    <label className="col-sm-2 col-form-label" htmlFor="title1"><h4>Title</h4></label>
+                                    <div className="col-sm-8">
+                                        <input className="form-control" type="text" id="title1" placeholder="Title" />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label className="col-sm-2 col-form-label" htmlFor="note"><h4>My Note</h4></label>
+                                    <div className="col-sm-8">
+                                        <textarea className="form-control noteDescription" id="note" type="text" placeholder="write your note here..." />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label className="col-sm-2 col-form-label" htmlFor="Tags"><h4>Tag</h4></label>
+                                    <div className="col-sm-8">
+                                    <input className="form-control" id="Tags" type="text" placeholder="#" />
+                                    </div>
+                                </div>
+                                <div className="inline-bt">
+                                    <button className="btn btn-success btn-lg" type="submit">Submit</button>
+                                </div>
+                                <div className="inline-bt">
+                                    <Link to='/notes'><button className="btn btn-danger btn-lg" type="button">Cancel</button></Link>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
-    submitNote (e) {
+        );
+    }
+    submitNote(e) {
         e.preventDefault();
         const newNoteObj = {
             title: e.target[0].value,
--- src/components/EditNote.js
+++ src/components/EditNote.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Redirect } from 'react-router-dom';
 
 
 class EditNote extends React.Component {
-    constructor (props) {
+    constructor(props) {
         super(props);
         this.state = {
             title: this.props.note.title,
@@ -21,65 +21,89 @@ class EditNote extends React.Component {
         this.editNoteSubmit = this.editNoteSubmit.bind(this);
         this.deleteNote = this.deleteNote.bind(this);
     }
-    render () {
+    render() {
         return (
             <div>
                 {this.state.justDeleted &&
-                    <Redirect to={'/notes/deleted'}/>
+                    <Redirect to={'/notes/deleted'} />
                 }
                 {this.state.justEdited &&
-                    <Redirect to={'/notes/edited'}/>
+                    <Redirect to={'/notes/edited'} />
                 }
-                <form onSubmit={this.editNoteSubmit}>
-                    <div>
-                        <label htmlFor="Title">Title</label>
-                        <br />
-                        <input className="titleInput" type="text" name="Title" onChange={this.titleChange} defaultValue={this.props.note.title} />
+                <div className='panel panel-default'>
+                    <div className="panel-heading">
+                        <h3 className="panel-title">Edit Note</h3>
                     </div>
-                    <div>
-                        <label htmlFor="note">My Note</label>
-                        <br />
-                        <textarea className="noteInput" name="note" type="text" onChange={this.textChange} defaultValue={this.props.note.text} />
+                    <div className="panel-body">
+                        <div className="container">
+                            <form onSubmit={this.editNoteSubmit}>
+                                <div className="form-group row">
+                                    <label className="col-sm-2 col-form-label" htmlFor="title2"><h4>Title</h4></label>
+                                    <div className="col-sm-8">
+                                        <input className="form-control" type="text" id="title2" onChange={this.titleChange} defaultValue={this.props.note.title} />
+                                    </div>
+                                </div>
+                                <div className="form-group row">
+                                    <label className="col-sm-2 col-form-label" htmlFor="note2"><h4>My Note</h4></label>
+                                    <div className="col-sm-8">
+                                        <textarea className="form-control noteDescription" id="note2" type="text" onChange={this.textChange} defaultValue={this.props.note.text} />
+                                    </div>
+                                </div>
+                                <div className="col-sm-8">
+                                        {this.state.tags.map((tag, i) => {
+                                            return (<div key={i}>
+                                                {
+                                                    tag.length > 0 &&
+                                                    <span>
+                                                        {tag}
+                                                        <i onClick={this.removeTag.bind(this, tag)} className="fa fa-times-circle-o" ></i>
+                                                    </span>
+                                                }
+                                            </div>
+                                            );
+                                        })}
+                                    <label className="col-sm-2 col-form-label" htmlFor="Tags2"><h4>Tag</h4></label>
+                                    <div className="col-sm-8">
+                                        <input className="form-control" id="Tags2" type="text" placeholder="#" />
+                                    </div>
+                                    <div className="container-fluid">
+                                    </div>
+                                </div>
+                                <br />
+                                <br />
+                                <br />
+                                <div className="btn-group">
+                                    <div className="inline-bt">
+                                        <button className="btn btn-success" type="submit">Save Changes</button>
+                                    </div>
+                                    <div className="inline-bt">
+                                        <button className="btn btn-warning" onClick={this.props.editNote} type="button">Cancel</button>
+                                    </div>
+                                    <div className="inline-bt">
+                                        <button className="btn btn-danger" onClick={this.deleteNote.bind(null, this.props.id)} type="button">Delete</button>
+                                    </div>
+
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div>
-                        <label htmlFor="Tags">#tag</label>
-                        <br />
-                        {this.state.tags.map((tag, i) => {
-                            return (<div key={i}>
-                                {
-                                    tag.length > 0 && 
-                                <span>
-                                    {tag}
-                                    <i onClick={this.removeTag.bind(this, tag)} className="fa fa-times-circle-o" ></i>
-                                </span>
-                                }
-                            </div>
-                            );
-                        })}
-                        <input name="Tags" type="text" placeholder="#" />
-                    </div>
-                    <div>
-                        <button className="btn btn-success" type="submit">Save Changes</button>
-                        <button className="btn btn-warning" onClick={this.props.editNote} type="button">Cancel</button>
-                        <button className="btn btn-danger" onClick={this.deleteNote.bind(null, this.props.id)} type="button">Delete</button>
-                    </div>
-                </form>
+                </div>
             </div>
         );
     }
-    titleChange (e) {
+    titleChange(e) {
         e.preventDefault();
         this.setState({
             title: e.target.value
         });
     }
-    textChange (e) {
+    textChange(e) {
         e.preventDefault();
         this.setState({
             title: e.target.value
         });
     }
-    removeTag (tag) {
+    removeTag(tag) {
         const newTags = [...this.state.tags];
         const i = findIndex(newTags, tag);
         newTags.splice(i, 1);
@@ -87,7 +111,7 @@ class EditNote extends React.Component {
             tags: newTags
         });
     }
-    editNoteSubmit (e) {
+    editNoteSubmit(e) {
         e.preventDefault();
         const newTags = e.target[2].value.split(',').concat(this.state.tags);
         const editedNote = {
@@ -97,19 +121,19 @@ class EditNote extends React.Component {
             noteId: this.props.id
         };
         editNote(editedNote)
-        .then(() => {
-            console.log('noteEdited');
-            return (
-            this.setState({
-                justEdited: !this.state.justEdited
+            .then(() => {
+                console.log('noteEdited');
+                return (
+                    this.setState({
+                        justEdited: !this.state.justEdited
+                    })
+                );
             })
-            );
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+            .catch((err) => {
+                console.log(err);
+            });
     }
-    deleteNote (id) {
+    deleteNote(id) {
         // e.preventDefault();
         deleteNote(id)
             .then(() => {
@@ -125,7 +149,7 @@ class EditNote extends React.Component {
     }
 }
 
-function findIndex (tags, name) {
+function findIndex(tags, name) {
     for (let i = 0; i < tags.length; i++) {
         if (tags[i] === name) {
             return i;
